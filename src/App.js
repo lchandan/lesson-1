6575f613bd60298230f75d0a4d0219bde956799e
Chandan Lakkoju <lchandan@outlook.com>
2021-02-16 13:37:56 +0530

    reverting the {name} commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 
-let name = "Chandan Lakkoju"
-
 class App extends Component {
   render() {
-    return <div className='App'>Hello World {name}</div>;
+    return <div className='App'>Hello World</div>;
   }
 }
 
